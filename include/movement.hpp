f12caa80b4ff8016288237927e9d769aa8125037
tskopen <tskopen@gmail.com>
2026-05-19 17:59:17 -0400

    Add rayTraceAttack to hpp

--- include/movement.hpp
+++ include/movement.hpp
@@ -15,6 +15,9 @@ uint64_t generateKingAttacks(PiecePosition KING_POSITION);
 uint64_t pawnMovements(PiecePosition PAWN_POSITION, Color color);
 uint64_t pawnAttacks(PiecePosition PAWN_POSITION, Color color);
 
+uint64_t rayTraceAttack (PiecePosition startingSquare, int direction, int angle)
+
+
 uint64_t bishopSlide(PiecePosition BISHOP_POSITION);
 uint64_t rookSlide(PiecePosition ROOK_POSITION);
 uint64_t queenMovement(PiecePosition QUEEN_POSITION);
